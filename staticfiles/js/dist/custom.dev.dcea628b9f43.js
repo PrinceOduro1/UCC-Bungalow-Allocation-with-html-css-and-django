a5100fef20b7cd64b8c6df7dbd09cf9d8a9de37c
Jefest1 <anyanf101@gmail.com>
2024-08-26 15:40:40 +0000

    Frontend and static files update

---
+++ staticfiles/js/dist/custom.dev.dcea628b9f43.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Handle form navigation
+$(document).ready(function () {
+  $('.next-section').click(function () {
+    var nextTab = $(this).data('next');
+    $('#' + nextTab).tab('show');
+  });
+  $('.previous-section').click(function () {
+    var prevTab = $(this).data('previous');
+    $('#' + prevTab).tab('show');
+  });
+});
